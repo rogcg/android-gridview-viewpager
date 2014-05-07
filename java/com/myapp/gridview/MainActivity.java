c7a2153ed486de852beceba75fdf5bda6361ceca
rogcg <roger.camara@gmail.com>
2014-05-07 09:25:48 -0300

    removed unnecessary files

--- java/com/myapp/gridview/MainActivity.java
+++ java/com/myapp/gridview/MainActivity.java
@@ -174,31 +174,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
     }
 
-    public class SquareImageView extends ImageView
-    {
-        public SquareImageView(Context context)
-        {
-            super(context);
-        }
-
-        public SquareImageView(Context context, AttributeSet attrs)
-        {
-            super(context, attrs);
-        }
-
-        public SquareImageView(Context context, AttributeSet attrs, int defStyle)
-        {
-            super(context, attrs, defStyle);
-        }
-
-        @Override
-        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-        {
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-            setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
-        }
-    }
-
     private class MyAdapter extends BaseAdapter
     {
         private List<Item> items = new ArrayList<Item>();
