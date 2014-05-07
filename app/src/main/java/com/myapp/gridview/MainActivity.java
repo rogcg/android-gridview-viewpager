0462d428f2e6ad7cc39a4948e2d5d32ee2ff14f3
rogcg <roger.camara@gmail.com>
2014-05-07 09:14:39 -0300

    Initial commit

---
+++ app/src/main/java/com/myapp/gridview/MainActivity.java
@@ -0,0 +1,272 @@
+package com.myapp.gridview;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.myapp.gridview.R;
+
+
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener
+{
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Here we load the xml layout we created above
+        setContentView(R.layout.activity_main);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
+        {
+            @Override
+            public void onPageSelected(int position)
+            {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
+        {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
+
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
+
+        public SectionsPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return new PlaceholderFragment();
+        }
+
+        @Override
+        public int getCount()
+        {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            Locale l = Locale.getDefault();
+            switch (position)
+            {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a the gridview
+     */
+    public class PlaceholderFragment extends Fragment
+    {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // Here we inflate the layout we created above
+            GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+            gridView.setAdapter(new MyAdapter(MainActivity.this.getApplicationContext()));
+
+            return rootView;
+        }
+    }
+
+    public class SquareImageView extends ImageView
+    {
+        public SquareImageView(Context context)
+        {
+            super(context);
+        }
+
+        public SquareImageView(Context context, AttributeSet attrs)
+        {
+            super(context, attrs);
+        }
+
+        public SquareImageView(Context context, AttributeSet attrs, int defStyle)
+        {
+            super(context, attrs, defStyle);
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
+        }
+    }
+
+    private class MyAdapter extends BaseAdapter
+    {
+        private List<Item> items = new ArrayList<Item>();
+        private LayoutInflater inflater;
+
+        public MyAdapter(Context context)
+        {
+            inflater = LayoutInflater.from(context);
+
+            items.add(new Item("Image 1", Color.GREEN));
+            items.add(new Item("Image 2", Color.RED));
+            items.add(new Item("Image 3", Color.BLUE));
+            items.add(new Item("Image 4", Color.GRAY));
+            items.add(new Item("Image 5", Color.YELLOW));
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i)
+        {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i)
+        {
+            return items.get(i).colorId;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup)
+        {
+            View v = view;
+            ImageView picture;
+            TextView name;
+
+            if(v == null)
+            {
+                v = inflater.inflate(R.layout.gridview_item, viewGroup, false);
+                v.setTag(R.id.picture, v.findViewById(R.id.picture));
+                v.setTag(R.id.text, v.findViewById(R.id.text));
+            }
+
+            picture = (ImageView)v.getTag(R.id.picture);
+            name = (TextView)v.getTag(R.id.text);
+
+            Item item = (Item)getItem(i);
+
+            picture.setBackgroundColor(item.colorId);
+            name.setText(item.name);
+
+            return v;
+        }
+
+        private class Item
+        {
+            final String name;
+            final int colorId;
+
+            Item(String name, int drawableId)
+            {
+                this.name = name;
+                this.colorId = drawableId;
+            }
+        }
+    }
+}
